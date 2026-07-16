f7c029e8be49ad7bc414a34933e0fee04a881edf
Joni Lahtinen <joni.lahtinen@gmail.com>
2026-07-16 08:37:25 +0000

    Refactor accept method in VpnHandler to improve configuration handling

--- src/server.rs
+++ src/server.rs
@@ -7,24 +7,26 @@ use std::io;
 
 #[derive(Debug, Clone)]
 pub struct VpnHandler {
-    // Tallennetaan vain nimi, ei koko konfiguraatiota
     pub device_name: String,
 }
 
 impl ProtocolHandler for VpnHandler {
     async fn accept(&self, connection: Connection) -> Result<(), AcceptError> {
-        // Luodaan uusi konfiguraatio jokaisella kutsulla
+        // 1. Tehdään vaaralliset await-kutsut ensin, jolloin 'config' ei ole vielä olemassakaan
+        let (send, recv) = connection.accept_bi().await
+            .map_err(|e| AcceptError::from_err(io::Error::new(io::ErrorKind::Other, e.to_string())))?;
+
+        // 2. Luodaan 'config' ja 'dev' vasta AWAIT-kutsun JÄLKEEN
+        // Nyt config ei elä await-kutsun yli, joten se ei haittaa säieturvallisuutta
         let mut config = Configuration::default();
         config.name(&self.device_name);
 
-        let (send, recv) = connection.accept_bi().await
-            .map_err(|e| AcceptError::from_err(io::Error::new(io::ErrorKind::Other, e.to_string())))?;
-            
         let dev = tun::create_as_async(&config)
             .map_err(|e| AcceptError::from_err(io::Error::new(io::ErrorKind::Other, e.to_string())))?;
         
         println!("Uusi VPN-yhteys hyväksytty!");
         
+        // 3. Nyt voidaan aloittaa siltaus
         if let Err(e) = vpn::bridge(dev, send, recv).await {
             eprintln!("Yhteysvirhe: {:?}", e);
         }
